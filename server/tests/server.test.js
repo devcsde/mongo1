3d254d0eba17a2d6ff68c09d5af0b42b25bb9088
devcs <c.scheidler@gmx.net>
2017-07-24 03:46:32 +0200

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -13,7 +13,9 @@ const todos = [{
     text: "First test todo"
 },{
     _id: new ObjectID(),
-    text: "Second test todo"
+    text: "Second test todo",
+    completed: true,
+    completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -143,4 +145,56 @@ describe("DELETE /todos/:id", () => {
             .expect(404)
             .end(done);
     });
+});
+
+describe("PATCH /todos/:id", () => {
+    it("should update the todo", (done) => {
+        let hexId = todos[0]._id.toHexString();
+        let text = "This is the new text";
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA("number");
+            })
+            .end(done);
+        // grab id of first item
+        // update the text set completed = true
+        // 200
+        // res.body text change, completedAt is a no .toBeA
+
+    });
+
+    it("should clear completedAt when todo is not completed", (done) => {
+        let hexId = todos[1]._id.toHexString();
+        let text = "This is the other new text";
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done);
+        // grab id of second todo item
+        // update text, set completed to false
+        // 200
+        // text is changed compelted fales, completedAt is null .toNotExist
+
+    });
+
+
 });
