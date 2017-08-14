b06480506952091649a0f040246b4ed129df6e8a
devcsde <c.scheidler@gmx.net>
2017-08-15 01:22:49 +0200

    finished async

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,21 +1,21 @@
 /**
  * Created by csche on 24.07.2017.
  */
+
 let {User} = require("./../models/user");
 
-let authenticate = async (req, res, next)=> {
-    try {
-        let token = req.header("x-auth");
-        const user = await User.findByToken(token);
+let authenticate = (req, res, next)=> {
+    let token = req.header("x-auth");
+    User.findByToken(token).then((user) => {
         if (!user){
             return Promise.reject();
         }
         req.user = user;
         req.token = token;
         next();
-    } catch (e) {
+    }).catch((e) => {
         res.status(401).send();
-    }
+    });
 };
 
 module.exports = {authenticate};
